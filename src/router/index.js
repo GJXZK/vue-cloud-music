8963f7f869754b626da05af10e31b1e58f3ee161
GJXZK <2587579585@qq.com>
2023-05-07 17:35:17 +0800

    编写路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Index = () =>import('@/components/Index')
 
 Vue.use(VueRouter)
 
 const routes = [
+  {path:'/',redirect:'/index'},
+  {
+    path:'/index',
+    component:Index,
+  }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode:'history'
 })
 
 export default router
