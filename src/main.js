26181d91b7b66793ff819023110c2004b489c601
GJXZK <2587579585@qq.com>
2023-05-07 11:39:25 +0800

    准备工作

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element'
 import router from './router'
 import store from './store'
 
+// 全局样式表 
+import './assets/css/base.css'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
 new Vue({
