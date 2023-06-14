46e628fda34db89b62199f69931f7e17d20cc202
GJXZK <2587579585@qq.com>
2023-06-14 11:53:07 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    // proxy:{
-    //   '/api':{
-    //     target:'http://localhost:3000',
-    //     // target:'http://www.codeman.ink:3000',
-    //     changeOrigin:true,
-    //     ws:true,
-    //     pathRewrite:{
-    //       '^/api':''
-    //     }
-    //   }
-    // }
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        // target:'http://www.codeman.ink:3000',
+        changeOrigin:true,
+        ws:true,
+        pathRewrite:{
+          '^/api':''
+        }
+      }
+    }
   }
 })
 
